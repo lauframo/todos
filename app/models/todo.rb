fc9a210b9341c9c50d9c954ac3fca9b051f35c18
Laura Moreno <stereofonik.imgs@gmail.com>
2017-02-08 15:02:26 -0600

    create todos migration and model. tested succesfully on console

---
+++ app/models/todo.rb
@@ -0,0 +1,16 @@
+class Todo < ActiveRecord::Base
+  belongs_to :user
+
+  after_save :update_task
+  after_create :update_task
+
+  validates :task_name, :due_date, { presence: true }
+  # validates :task_name, uniqueness: { scope: :due_date }
+
+
+  def update_task
+  	self.complete = false
+  	return true
+  end
+
+end
