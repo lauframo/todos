23b0ac9dfbe4a8de6bb9ede244adb7794ee50c36
Laura Moreno <stereofonik.imgs@gmail.com>
2017-02-08 15:24:25 -0600

    update todo so it only runs after create

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,7 @@
 class Todo < ActiveRecord::Base
   belongs_to :user
 
-  after_save :update_task
+  # after_save :update_task
   after_create :update_task
 
   validates :task_name, :due_date, { presence: true }
@@ -12,5 +12,6 @@ class Todo < ActiveRecord::Base
   	self.complete = false
   	return true
   end
+
 
 end
